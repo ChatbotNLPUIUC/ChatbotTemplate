f62a98c3c399091d11985fbd67f3dc93e7841c28
Daniel Qian <danielqian2015@gmail.com>
2020-10-13 00:42:18 -0500

    Parsed through math directory

--- BeautifulSoupRunThrough/quoteBSoup.py
+++ BeautifulSoupRunThrough/quoteBSoup.py
@@ -9,24 +9,27 @@ r = requests.get(URL)
 
 soup = BeautifulSoup(r.content, 'html5lib')
 
-quotes=[] # a list to store quotes
+courses=[] # a list to store quotes
 
 table = soup.find('div', attrs = {'id':'courseinventorycontainer'})
 
-for row in table.findAll('div',
-						attrs = {'class':'courseblock'}):
-	#quote = {}
+for row in table.findAll('div', attrs = {'class':'courseblock'}):
+    course = {}
 	#quote['theme'] = row.h5.text
-	#quote['url'] = row.a['href']
-	#quote['img'] = row.img['src']
+    course['url'] = row.a['href']
+    course['name'] = row.a.getText()
+    courses.append(course)
+    print(row)
 	#quote['lines'] = row.img['alt'].split(" #")[0]
 	#quote['author'] = row.img['alt'].split(" #")[1]
-	#quotes.append(quote)
-    print(row.a['href'])
 
-#filename = 'inspirational_quotes.csv'
-#with open(filename, 'w', newline='') as f:
-	#w = csv.DictWriter(f,['theme','url','img','lines','author'])
-	#w.writeheader()
-	#for quote in quotes:
-		#w.writerow(quote)
+
+#'theme','url','img','lines','author'
+
+filename = 'uiuc_math.csv'
+
+with open(filename, 'w', newline='') as f:
+	w = csv.DictWriter(f,['url', 'name'])
+	w.writeheader()
+	for course in courses:
+		w.writerow(course)
