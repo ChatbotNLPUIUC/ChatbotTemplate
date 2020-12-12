dd15e63fda167b3643e3c8af71eb42737cecb384
Daniel Qian <danielqian2015@gmail.com>
2020-12-12 00:41:52 -0800

    Adding in whoosh files

--- python-project-chatbot-codes/parseJSON.py
+++ python-project-chatbot-codes/parseJSON.py
@@ -1,26 +1,44 @@
 import json
 import numpy as np
 
-with open("betterIntents.json") as f:
-    data = json.load(f)
+def doingJSON(filename):
+    with open(filename) as f:
+        data = json.load(f)
 
-new_data = []
+    new_data = []
 
-for intent in data["intents"]:
-    if not new_data:
-        print(intent)
-    datum = {}
-    datum["tag"] = intent["intent"]
-    datum["patterns"] = intent["text"]
-    datum["responses"] = intent["responses"]
-    datum["context"] = [""]
-    new_data.append(datum)
+    for intent in data["intents"]:
+        if not new_data:
+            print(intent)
+        datum = {}
+        datum["tag"] = intent["intent"]
+        datum["patterns"] = intent["text"]
+        datum["responses"] = intent["responses"]
+        datum["context"] = [""]
+        new_data.append(datum)
+    return new_data
+    #intent = {}
+    #intent['intents'] = new_data
 
-intent = {}
-intent['intents'] = new_data
+def listJson(filename):
+    with open(filename) as f:
+        data = json.load(f)
 
-filename = 'test_conversation.json'
-json_obj = json.dumps(intent, indent = 4)
+    new_data = []
 
-with open(filename, 'w',) as outfile:
-	outfile.write(json_obj)
+    for intent in data["intents"]:
+        if not new_data:
+            print(intent)
+        datum = {}
+        datum["tag"] = intent["tag"]
+        datum["patterns"] = '.'.join(intent["patterns"])
+        datum["responses"] = intent["responses"]
+        datum["context"] = [""]
+        new_data.append(datum)
+    return new_data
+#filename = 'test_conversation.json'
+
+def writeFile(filename, intent):
+    json_obj = json.dumps(intent, indent = 4)
+    with open(filename, 'w',) as outfile:
+	    outfile.write(json_obj)
---
+++ python-project-chatbot-codes/whooshtrial.py
@@ -0,0 +1,63 @@
+from whoosh.index import create_in
+from whoosh.fields import *
+from whoosh import index
+import os.path
+from whoosh.qparser import QueryParser
+import sys
+import os
+from parseJSON import doingJSON, listJson
+    
+class whooshFinder:
+    def __init__(self):
+        if not os.path.exists("indexdir"):
+            os.mkdir("indexdir")
+        global schema
+        global ix
+        schema = Schema(title=TEXT(stored=True), path=ID(stored=True), content=TEXT(stored = True))
+        ix = index.create_in("indexdir", schema)
+
+        writer = ix.writer()
+        temp = ""
+        doingsplit = listJson("test_math.json")
+        for i in doingsplit:
+            writer.add_document(content=i["patterns"])
+        writer.commit()
+        
+
+    def whooshFind(self, check):
+        endpoint = []
+        scores = []
+        total = 0.0
+        if check != "":
+            with ix.searcher() as searcher:
+                query = QueryParser("content", ix.schema).parse(check)
+                results = searcher.search(query)
+        
+                for r in results:
+                    endpoint.append(r['content'].replace('\n', ''))
+                    #print (r, r.score)
+                    scores.append(r.score)
+                    total += r.score
+                    # Was this results object created with terms=True?
+                    #if results.has_matched_terms():
+                        # What terms matched in the results?
+                        #print(results.matched_terms())
+                if len(endpoint) == 0:
+                    return ["No Sentences Found."]
+                average = (float)(total/len(endpoint))
+                # What terms matched in each hit?
+        together = []
+        for i in range(len(endpoint)):
+            if scores[i] > average:
+                together = together + endpoint[i].split(".")
+                together.remove('')
+        return endpoint[0]
+
+if __name__ == "__main__":
+    find = whooshFinder()
+    while True:
+        check = input ("Enter keywords: ")
+        if check == 'stop':
+            break
+        print(find.whooshFind(check))
+        #print(listJson("test_math.json"))
